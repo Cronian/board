c2282ed08370e93f056aa918e56a71f03013b003
Cronian <cronian89@gmail.com>
2023-03-23 17:23:27 +0900

    feat thymeleaf layout template
    1. 타임리프 레이아웃 설정 및 프레그먼트 설정
    2. 테일윈드 CSS 적용
    3. 에러페이지 ( 404 ) 설정

--- src/main/java/com/cronian/board/config/MvcConfiguration.java
+++ src/main/java/com/cronian/board/config/MvcConfiguration.java
@@ -1,8 +1,21 @@
 package com.cronian.board.config;
 
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Configuration
 public class MvcConfiguration {
 
+    /**
+     * Thymeleaf layout처리를 위한 셋팅
+     * @return templateEngine;
+     */
+    /*@Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addDialect(new LayoutDialect());
+        return templateEngine;
+    }*/
 }
