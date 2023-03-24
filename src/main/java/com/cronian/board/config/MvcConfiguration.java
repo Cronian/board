e610cd2a8baffb324c793315d0c4af117ec160da
Cronian <cronian89@gmail.com>
2023-03-24 16:06:30 +0900

    feat header.html
    1. 헤더 tailwind.css가 적용된 스타일 소스 가져옴
    2. 구현목적에 맞게 수정

--- src/main/java/com/cronian/board/config/MvcConfiguration.java
+++ src/main/java/com/cronian/board/config/MvcConfiguration.java
@@ -8,14 +8,4 @@ import org.thymeleaf.spring6.SpringTemplateEngine;
 @Configuration
 public class MvcConfiguration {
 
-    /**
-     * Thymeleaf layout처리를 위한 셋팅
-     * @return templateEngine;
-     */
-    /*@Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.addDialect(new LayoutDialect());
-        return templateEngine;
-    }*/
 }
